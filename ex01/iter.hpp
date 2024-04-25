f365496ff013643e1ea8073af55d5b0024575bd7
sboulal <rawanasmaa51@gmail.com>
2024-04-25 22:21:48 +0100

    add ex01 is done

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,10 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saboulal <saboulal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: saboulal <saboulal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:47:08 by saboulal          #+#    #+#             */
-/*   Updated: 2024/04/25 16:47:09 by saboulal         ###   ########.fr       */
+/*   Updated: 2024/04/25 22:20:44 by saboulal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+template <typename T>
+void iter(T *array, size_t size, void (*func)(T const &))
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        func(array[i]);
+    }
+}
+
+template <typename T>
+void print(T const &x)
+{
+    std::cout << x << std::endl;
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,10 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: saboulal <saboulal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: saboulal <saboulal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:47:11 by saboulal          #+#    #+#             */
-/*   Updated: 2024/04/25 16:47:12 by saboulal         ###   ########.fr       */
+/*   Updated: 2024/04/25 22:21:13 by saboulal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+#define N 5
+
+int main( void )
+{
+    int arr1[] = { 1, 2, 3, 4, 5 };
+
+    std::cout << "arr1: " << std::endl;
+    iter(arr1, N, &print);
+
+    double arr2[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+
+    std::cout << "\narr2: " << std::endl;
+    iter(arr2, N, &print);
+
+    char arr3[] = { 'a', 'b', 'c', 'd', 'e' };
+
+    std::cout << "\narr3: " << std::endl;
+    iter(arr3, N, &print);
+
+    std::string arr4[] = { "one", "two", "three", "four", "five" };
+
+    std::cout << "\narr4: " << std::endl;
+    iter(arr4, N, &print);
+
+    return 0;
+}
